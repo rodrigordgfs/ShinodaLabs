2986fb546766eaa0c9b5b89f70b347099c2792fc
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2024-08-05 18:07:35 -0300

    feat(app): nova versão do portifólio
    Arquivos iniciais do projeto para a nova versão do portifólio

--- src/components/Background/index.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Background() {
-  return (
-    <div className="w-full h-[400px] bg-hero shadow-md object-cover bg-cover bg-center relative">
-      <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent" />
-    </div>
-  );
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero": "url('/background.jpg')",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
