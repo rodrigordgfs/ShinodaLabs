4e33e32dd2abecaf1c051c776edf7bc2cc347c38
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2024-08-09 14:52:27 -0300

    feat(app): adicionado botçao de CTA na home

--- src/app/[locale]/components/HomeContent.tsx
+++ src/app/[locale]/components/HomeContent.tsx
@@ -20,6 +20,9 @@ export default function HomeContent() {
           showCursor={true}
         />
       </p>
+      <button className="bg-lime-500 text-zinc-950 px-6 py-2 w-full md:w-auto rounded-full hover:bg-lime-400 transition-all">
+        {t("quote")}
+      </button>
     </main>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-  locales: ['en', 'es', 'br'],
+  locales: ['br', 'en', 'es'],
   defaultLocale: 'br'
 });
 
